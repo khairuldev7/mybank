c82022be28688cac7ee899571239b5e8bc42e1b4
Khair <Khair@10.0.0.52>
2023-02-01 13:12:38 -0600

    successfully completed

--- src/practice/welcome.java
+++ src/practice/welcome.java
@@ -7,6 +7,8 @@ public class welcome {
 		System.out.println("Welcome to DevOps Training");
 		System.out.println("by Khairul");
 		System.out.println(40*30);
+		System.out.println("Hurray we did it");
+		
 
 	}
 
