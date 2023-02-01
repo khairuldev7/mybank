44bb49079f9ef77d9eac6e1f5281b8ca392eec7c
Khair <Khair@10.0.0.52>
2023-02-01 13:06:32 -0600

    i am uploading my first app

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Khair
+ *
+ */
+module MyApp {
+}
---
+++ src/practice/welcome.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class welcome {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to DevOps Training");
+		System.out.println("by Khairul");
+		System.out.println(40*30);
+
+	}
+
+}
