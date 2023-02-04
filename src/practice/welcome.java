7656c6d8651dbe354fb0f33d68e3385914311d5a
Khair <Khair@10.0.0.52>
2023-02-04 02:10:07 -0600

    APP IS OUT OF SERVICE

--- src/practice/welcome.java
+++ src/practice/welcome.java
@@ -4,18 +4,18 @@ package practice;
 
 public class welcome {
 
-	public static void main(String[] args) {
+
 		
 		System.out.println("Welcome to DevOps Training");
 		System.out.println("by Khairul");
 		System.out.println(40*30);
 		System.out.println("Hurray we did it");
 		System.out.println(3520-20);
-		
-
-	
-	
-
-	}
+		System.out.println(250*56);
+		System.out.println(200-350);
+		System.out.println("MSi discount");
+	System.out.println("Devops has done his job");
+	System.out.println(450-9560);
+	}System.out.println("gooday");
 
 }
